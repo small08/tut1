180b0b0ed62c0140d403db0238299d453594ea05
Ryan <ryanhoward351@gmail.com>
2023-07-30 22:15:36 +1200

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -17,6 +17,9 @@ public class Greeter {
   }
 
   public static void main(String[] args) {
+
+    System.out.println("Bug fix");
+
     String language = args[0];
     String country = args[1];
     String name = args[2];
